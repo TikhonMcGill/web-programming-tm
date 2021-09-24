76f0479ce908ea4789683efdb77d67f1f9a6bce7
TikhonMcGill <tixon2002@gmail.com>
2021-09-24 11:33:54 +0100

    Changes:
    1. Add all scripts from the pages into a "scripts.js" file
    2. Reference scripts.js in all pages' <script> tags, instead of writing the script in page

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function go_to_explanation() {
+	window.location.href = "pages/tikhon.html";
+}
+
+function go_to_tychonia() {
+	window.location.href = "pages/tychonia.html";
+}
+
+function backToMenu() {
+	window.location.href = "../index.html";
+}
